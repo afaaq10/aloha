375dee03f88513410727edf77dc9ccbf408e9436
afaaq10 <afaaqmajeed123@gmail.com>
2024-03-25 10:52:16 +0530

    renamed number of digits

--- src/App.tsx
+++ src/App.tsx
@@ -18,23 +18,23 @@ const App = () => {
     const [sum, setSum] = React.useState<number | null>(null);
     const [isStarted, setIsStarted] = React.useState<boolean>(false);
     const [speed, setSpeed] = React.useState<number>(1500);
-    const [numberOfDigits, setNumberOfDigits] = React.useState<number>(6);
+    const [numberOfRows, setNumberOfRows] = React.useState<number>(6);
 
     React.useEffect(() => {
-        if (currentDigitIndex === numberOfDigits) {
+        if (currentDigitIndex === numberOfRows) {
             const newSum = numbers.reduce((acc, curr) => acc + curr, 0);
             setSum(newSum);
             return;
         }
 
-        if (isStarted && currentDigitIndex < numberOfDigits) {
+        if (isStarted && currentDigitIndex < numberOfRows) {
             const timeout = setTimeout(() => {
                 setCurrentDigitIndex(prevIndex => prevIndex + 1);
             }, speed);
 
             return () => clearTimeout(timeout);
         }
-    }, [currentDigitIndex, isStarted, numbers, speed, numberOfDigits]);
+    }, [currentDigitIndex, isStarted, numbers, speed, numberOfRows]);
 
     const startProcess = () => {
         setCurrentDigitIndex(0);
@@ -43,7 +43,7 @@ const App = () => {
         const newNumbers = [generateRandomNumbers()];
         let previousNumber = newNumbers[0];
 
-        for (let i = 1; i < numberOfDigits; i++) {
+        for (let i = 1; i < numberOfRows; i++) {
             let newNumber;
 
             if (previousNumber >= 0) {
@@ -90,12 +90,12 @@ const App = () => {
                 )}
                 {((!isStarted && sum === null) || sum !== null) && (
                     <div className="mt-2">
-                        <label htmlFor="digitsInput" className="text-white">Number of Digits:</label>
+                        <label htmlFor="digitsInput" className="text-white">Number of Rows:</label>
                         <input
                             id="digitsInput"
                             type="number"
-                            value={numberOfDigits}
-                            onChange={(e) => setNumberOfDigits(parseInt(e.target.value))}
+                            value={numberOfRows}
+                            onChange={(e) => setNumberOfRows(parseInt(e.target.value))}
                             min="1"
                             max="10"
                             className="block w-full px-2 py-1 mt-1 text-gray-800 bg-gray-200 rounded-md focus:outline-none focus:ring focus:ring-indigo-200"
