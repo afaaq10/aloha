21e44fd3002a543e34e57db35bd927b0fce0aa20
afaaq10 <afaaqmajeed123@gmail.com>
2024-03-27 17:53:57 +0530

    :lipstick: Added cards

--- src/App.tsx
+++ src/App.tsx
@@ -14,19 +14,30 @@ import Decimal from './views/Decimal';
 import Square from './views/Square';
 import SquareRoot from './views/SquareRoot';
 
-const App = () => {
+interface OperationCardProps {
+    operationName: string;
+    onClick: () => void;
+}
 
+const OperationCard: React.FC<OperationCardProps> = ({ operationName, onClick }) => (
+    <div className="p-4 transition duration-300 transform bg-white shadow-md cursor-pointer w-44 rounded-xl hover:shadow-lg hover:scale-105" onClick={onClick}>
+        <p className="font-normal text-center text-black ">{operationName}</p>
+    </div>
+);
+
+const App = () => {
     const [selectedOperation, setSelectedOperation] = React.useState('');
 
     return (
         <div className='flex flex-col items-center min-h-screen bg-[#0f172a]'>
             <div className='flex flex-col gap-2 mt-7'>
-                <p className='text-4xl font-medium text-white'>Aloha Rush </p>
-                <p className='text-xl text-center text-white'>by <span className='text-green-200'>Afaaq Majeed</span> </p>
+                <p className='text-4xl font-medium text-white'>Aloha Rush</p>
+                <p className='text-xl text-center text-white'>by <span className='text-green-200'>Afaaq Majeed</span></p>
             </div>
+
             {selectedOperation ? (
                 <>
-                    {selectedOperation === 'Add/Subtract' && <AddSub />}
+                    {selectedOperation === 'Addition/Subtraction' && <AddSub />}
                     {selectedOperation === 'Multiplication' && <Multiplication />}
                     {selectedOperation === 'Division' && <Division />}
                     {selectedOperation === 'Decimal' && <Decimal />}
@@ -36,22 +47,23 @@ const App = () => {
             ) : (
                 <div className='flex flex-col mt-24 gap-14'>
                     <div className="flex gap-16">
-                        <button onClick={() => setSelectedOperation('Add/Subtract')} className='w-40 px-4 py-2 text-black bg-green-400 rounded-xl hover:scale-105'>Add/Subtract</button>
-                        <button onClick={() => setSelectedOperation('Multiplication')} className='w-40 px-4 py-2 text-black bg-green-400 rounded-xl hover:scale-105'>Multiplication</button>
+                        <OperationCard operationName="Addition/Subtraction" onClick={() => setSelectedOperation('Addition/Subtraction')} />
+                        <OperationCard operationName="Multiplication" onClick={() => setSelectedOperation('Multiplication')} />
                     </div>
 
                     <div className="flex gap-16">
-                        <button onClick={() => setSelectedOperation('Division')} className='w-40 px-4 py-2 text-black bg-green-400 rounded-xl hover:scale-105'>Division</button>
-                        <button onClick={() => setSelectedOperation('Decimal')} className='w-40 px-4 py-2 text-black bg-green-400 rounded-xl hover:scale-105'>Decimal</button>
+                        <OperationCard operationName="Division" onClick={() => setSelectedOperation('Division')} />
+                        <OperationCard operationName="Decimal" onClick={() => setSelectedOperation('Decimal')} />
                     </div>
 
                     <div className="flex gap-16">
-                        <button onClick={() => setSelectedOperation('Square')} className='w-40 px-4 py-2 text-black bg-green-400 rounded-xl hover:scale-105'>Square</button>
-                        <button onClick={() => setSelectedOperation('SquareRoot')} className='w-40 px-4 py-2 text-black bg-green-400 rounded-xl hover:scale-105'>Square root</button>
+                        <OperationCard operationName="Square" onClick={() => setSelectedOperation('Square')} />
+                        <OperationCard operationName="Square Root" onClick={() => setSelectedOperation('SquareRoot')} />
                     </div>
                 </div>
             )}
-            <div className='flex flex-col items-center justify-center flex-grow' style={{ justifyContent: 'start', marginTop: 74 }}> </div>
+
+            <div className='flex flex-col items-center justify-center flex-grow' style={{ justifyContent: 'start', marginTop: 74 }}></div>
             <footer className='py-4 text-sm text-white'>&copy; Afaaq Majeed</footer>
         </div>
     );
