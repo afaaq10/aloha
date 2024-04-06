ec78e00ea93fd154e5949e0c4f08ffb7560880f3
afaaq10 <afaaqmajeed123@gmail.com>
2024-04-06 20:16:43 +0530

    modified title

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ const App = () => {
     return (
         <div className='flex flex-col items-center min-h-screen bg-[#0f172a]'>
             <div className='flex flex-col gap-2 mt-7'>
-                <p className='text-4xl font-medium text-white'>Calculation Station</p>
+                <p className='text-4xl font-medium text-white'>Speedy Sums</p>
                 <p className='text-xl text-center text-white'>by <span className='text-green-200'>Afaaq Majeed</span></p>
             </div>
 
